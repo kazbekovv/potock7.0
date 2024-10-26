4cdbc13e28557810f82d0bc6810fb8c1931d4601
kazbekovv <ikazbekov21@icloud.com>
2024-10-26 18:39:49 +0600

    2

--- Todo/tasks/views.py
+++ Todo/tasks/views.py
@@ -2,9 +2,11 @@ from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
+
 class TaskCreateApiView(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
 
 class TaskDetailApiView(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()

8a9f1e2df41927ef0aa8b2aff818e582dffaa19c
kazbekovv <ikazbekov21@icloud.com>
2024-10-26 18:37:26 +0600

    2

--- Todo/tasks/serializers.py
+++ Todo/tasks/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Task
 
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = 'идентифекатор завершен'.split()
+        fields = ['id', 'completed']
